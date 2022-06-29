e1f2994867c939e182ee257a0d1cdda8a4b3ad27
Guilheermeap <guilhermeeap63@gmail.com>
2022-06-29 11:39:55 -0300

    29 - 06 - 2022

---
+++ Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Guilherme", 21));
+console.log(mostraHobby("Guilherme", "Jogar"));
+console.log(mostraCidade("Guilherme", "São Paulo"));
